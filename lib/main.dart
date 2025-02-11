9daffd1966b2a35d25f28026b5cfab96c647c89e
Hiromu0319 <green.0319.2001@gmail.com>
2025-02-11 23:07:21 +0900

    setup: firebase init stg

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'app.dart';
+import 'firebase_options.dart';
 import 'providers/package_info_instance/package_info_instance.dart';
 import 'providers/shared_preferences_instance/shared_preferences_instance.dart';
 
 void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await initSharedPreferencesInstance();
   await initPackageInfoInstance();
